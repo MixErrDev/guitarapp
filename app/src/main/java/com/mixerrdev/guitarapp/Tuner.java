a0f311d59d57eecf5b6311971f11fa7784f6f9b4
Mikhail Ermakov <miahermak@mail.ru>
2022-05-09 15:15:48 +0700

    simple

--- app/src/main/java/com/mixerrdev/guitarapp/Tuner.java
+++ app/src/main/java/com/mixerrdev/guitarapp/Tuner.java
@@ -10,75 +10,21 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 public class Tuner extends AppCompatActivity {
     int STORAGE_PERMISSION_CODE = 1;
 
-
     final String TAG = "myLogs";
-
-    int myBufferSize = 8192;
     AudioRecord audioRecord;
-    boolean isReading = false;
-
-    Button startRecord, stopRecord, startRead, stopRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tuner);
-
-        // Hiding Navigation Bar
-        ModsUI.hide(this);
+        setContentView(R.layout.activity_main);
 
         createAudioRecorder();
+
         Log.d(TAG, "init state = " + audioRecord.getState());
-
-        startRecord = findViewById(R.id.startRecord);
-        stopRecord = findViewById(R.id.stopRecord);
-        startRead = findViewById(R.id.startRead);
-        stopRead = findViewById(R.id.stopRead);
-
-        startRecord.setOnClickListener((View v) -> {
-            Log.d(TAG, "record start");
-            audioRecord.startRecording();
-            int recordingState = audioRecord.getRecordingState();
-            Log.d(TAG, "recordingState = " + recordingState);
-        });
-
-        stopRecord.setOnClickListener((View v) -> {
-            Log.d(TAG, "record stop");
-            audioRecord.stop();
-        });
-
-        startRead.setOnClickListener((View v) -> {
-            Log.d(TAG, "read start");
-            isReading = true;
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    if (audioRecord == null)
-                        return;
-
-                    byte[] myBuffer = new byte[myBufferSize];
-                    int readCount = 0;
-                    int totalCount = 0;
-                    while (isReading) {
-                        readCount = audioRecord.read(myBuffer, 0, myBufferSize);
-                        totalCount += readCount;
-                        Log.d(TAG, "readCount = " + readCount + ", totalCount = "
-                                + totalCount);
-                    }
-                }
-            }).start();
-        });
-
-        stopRead.setOnClickListener((View v) -> {
-            Log.d(TAG, "read stop");
-            isReading = false;
-        });
     }
 
     void createAudioRecorder() {
@@ -86,38 +32,15 @@ public class Tuner extends AppCompatActivity {
         int channelConfig = AudioFormat.CHANNEL_IN_MONO;
         int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
 
-        int minInternalBufferSize = AudioRecord.getMinBufferSize(sampleRate,
-                channelConfig, audioFormat);
+        int minInternalBufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
         int internalBufferSize = minInternalBufferSize * 4;
+
         Log.d(TAG, "minInternalBufferSize = " + minInternalBufferSize
                 + ", internalBufferSize = " + internalBufferSize
-                + ", myBufferSize = " + myBufferSize);
+                + ", myBufferSize = " + internalBufferSize);
 
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
             audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, internalBufferSize);
-        } else {
-            requestStoragePermission();
-        }
-    }
-
-    private void requestStoragePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
-
-        } else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, STORAGE_PERMISSION_CODE);
-        }
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        isReading = false;
-        if (audioRecord != null) {
-            audioRecord.release();
         }
     }
 }
