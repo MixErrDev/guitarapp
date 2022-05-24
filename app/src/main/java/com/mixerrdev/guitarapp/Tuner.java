4c611ae2cc90ed39ed4a1b624ba8860618af65bc
Mikhail Ermakov <miahermak@mail.ru>
2022-05-25 02:42:26 +0700

    reload

--- app/src/main/java/com/mixerrdev/guitarapp/Tuner.java
+++ app/src/main/java/com/mixerrdev/guitarapp/Tuner.java
@@ -1,15 +1,36 @@
 package com.mixerrdev.guitarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 
 public class Tuner extends AppCompatActivity {
 
-    AudioProc audioProc;
     TextView result;
+
+
+    int sampleRate = 8000;
+    int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
+    int minInternalBufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+    int myBufferSize = minInternalBufferSize * 2;
+
+
+    boolean isReading;
+
+    final String TAG = "myLogs";
+    AudioRecord audioRecord;
 
 
     @Override
@@ -17,20 +38,20 @@ public class Tuner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuner);
 
+
         // Hiding Navigation Bar
         ModsUI.hide(this);
         // Keeping screen on
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         result = findViewById(R.id.result);
-        audioProc = new AudioProc(this);
 
         // Start recording
-        audioProc.createAudioRecorder();
-        audioProc.recordStart();
+        createAudioRecorder();
+        recordStart();
 
         // Start reading PCM data and getting frequency
-        audioProc.readStart();
+        readStart();
     }
 
 
@@ -38,33 +59,91 @@ public class Tuner extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        audioProc.readStop();
-        audioProc.recordStop();
+        readStop();
+        recordStop();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        audioProc.readStop();
-        audioProc.recordStop();
+        readStop();
+        recordStop();
     }
 
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        audioProc.recordStart();
-        audioProc.readStart();
+        recordStart();
+        readStart();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        audioProc.readStop();
-        audioProc.recordStop();
+        readStop();
+        recordStop();
 
-        audioProc.release();
+        release();
+    }
+
+
+    public void createAudioRecorder() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, myBufferSize);
+        }
+    }
+
+    public void recordStart() {
+        Log.d(TAG, "record start");
+        audioRecord.startRecording();
+        int recordingState = audioRecord.getRecordingState();
+        Log.d(TAG, "recordingState = " + recordingState);
+    }
+
+    public void readStart() {
+        Log.d(TAG, "read start");
+        isReading = true;
+        new Thread(() -> {
+
+            if (audioRecord == null)
+                return;
+
+            short[] myBuffer = new short[myBufferSize];
+
+            while (isReading) {
+                // Reading PCM in myBuffer
+                audioRecord.read(myBuffer, 0, myBufferSize);
+
+                // Getting frequency from PCM
+                FrequencyScanner fft = new FrequencyScanner();
+                double[] rst = fft.extractFrequencyMagnitude(myBuffer, sampleRate);
+
+                // Show frequency
+                runOnUiThread(() -> result.setText(Double.toString(rst[0])));
+
+                Log.d(TAG, "frequency = " + rst[0] + "; magnitude = " + rst[1]);
+
+            }
+        }).start();
+    }
+
+    public void readStop() {
+        Log.d(TAG, "read stop");
+        isReading = false;
+    }
+
+    public void recordStop() {
+        Log.d(TAG, "record stop");
+        audioRecord.stop();
+    }
+
+    public void release() {
+        if (audioRecord != null) {
+            audioRecord.release();
+        }
     }
 }
